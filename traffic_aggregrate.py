733fcd720b9e9c6aa1b4475de71ce5fd6bf64fa9
Somya Mohanty <mohanty.somya@gmail.com>
2014-04-16 16:15:08 -0500

    Updated traffic aggregate

--- traffic_aggregrate.py
+++ traffic_aggregrate.py
@@ -4,25 +4,50 @@ import csv
 from bson.code import Code
 from pymongo import MongoClient
 import csv
+import sys
 
-          
+class mongo_host(object):
+    def __init__(self,mongo_db):
+        self.client = MongoClient(mongo_db['host'], mongo_db['default_port'])
+        self.db = self.client[mongo_db['db']]
+        self.collection = self.db[mongo_db['collection']]
+        self.time_field = '$' + mongo_db['time_field']
+
+    def get_traffic(self):
+        try:
+            tw_stuff = self.collection.aggregate([
+                {"$group":{
+                    "_id": {
+                        "y": { "$year": self.time_field },
+                        "m": { '$month': self.time_field },
+                        "d": { "$dayOfMonth": self.time_field },
+                        #h: { '$hour': self.time_field },
+                        #i: { '$minute': self.time_field },
+                    },
+                    "count": {"$sum": 1},
+                    }
+                }
+                ])
+            return tw_stuff
+        except:
+            return False
+
+
 if __name__ == '__main__':
-    client = MongoClient()
-    db = client.twitter
-    tw_traffic = db.twitter_geo_indexed
+    total = len(sys.argv)
 
-    tw_stuff = db.twitter_geo_indexed.aggregate([
-        {"$group":{
-            "_id": {
-                "y": { "$year": "$date" },
-                "m": { '$month': "$date" },
-                "d": { "$dayOfMonth": "$date" },
-                #h: { '$hour': '$date' },
-                #i: { '$minute': '$date' },
-            },
-            "count": {"$sum": 1},
-            }
-        }
-        ])
+    if total < 4:
+        print "Utilization: python traffic_aggregrate.py <mongo_host> <mongo_db> <mongo_collection> <time_field>"
+        exit(0)
 
-    print tw_stuff
+    mongo_db = {
+        'host' : str(sys.argv[1]),
+        'default_port' : 27017,
+        'db' : str(sys.argv[2]),
+        'collection' : str(sys.argv[3]),
+        'time_field' : str(sys.argv[4]),
+    }
+    
+    conn = mongo_host(mongo_db)
+
+    print conn.get_traffic()
