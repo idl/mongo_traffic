28a1ca04ec0c8df2a6613d4a79c91a9596754a3d
Somya Mohanty <mohanty.somya@gmail.com>
2014-04-18 13:29:32 -0500

    exporting geo

--- mongo_search.py
+++ mongo_search.py
@@ -21,7 +21,7 @@ class mongo_host(object):
         try:
             for doc in self.collection.find({"geo.type":"Point"}):
                 result = {
-                    'time' : doc['created_at'],
+                    'time' : doc['postedTime'],
                     'geo' : doc['geo']['coordinates']
                 }
                 yield result   
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     total = len(sys.argv)
 
     if total < 4:
-        print "Utilization: python traffic_aggregrate.py <mongo_host> <mongo_db> <mongo_collection> <query_field>"
+        print "Utilization: python mongo_search.py <mongo_host> <mongo_db> <mongo_collection> <query_field> <output_file>"
         exit(0)
 
     mongo_db = {
@@ -45,5 +45,14 @@ if __name__ == '__main__':
     
     conn = mongo_host(mongo_db)
 
-    for result in conn.get_data():
-        print result
+    count = 1
+    with open(sys.argv[5], 'wb') as csvfile:
+        mapwriter = csv.writer(csvfile)
+        mapwriter.writerow(['latitude','longitude', 'created_at'])
+
+        for result in conn.get_data():
+            #print result
+            mapwriter.writerow([result['geo'][1],result['geo'][0],result['time']])
+            count += 1
+            if count % 500 == 0:
+                print "Found and wrote: %d" % count
