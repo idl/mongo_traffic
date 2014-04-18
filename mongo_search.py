9fb3eb278b0d12977425113cc97857bf8d483bc1
Somya Mohanty <mohanty.somya@gmail.com>
2014-04-18 09:50:47 -0500

    Search init

---
+++ mongo_search.py
@@ -0,0 +1,51 @@
+import json
+import datetime
+import csv
+from bson.code import Code
+from pymongo import MongoClient
+import csv
+import sys
+import matplotlib.dates as dt
+import matplotlib.pyplot as plt
+import numpy as np
+from operator import itemgetter
+
+class mongo_host(object):
+    def __init__(self,mongo_db):
+        self.client = MongoClient(mongo_db['host'], mongo_db['default_port'])
+        self.db = self.client[mongo_db['db']]
+        self.collection = self.db[mongo_db['collection']]
+        self.query_field = '$' + mongo_db['query_field']
+
+    def get_data(self):
+        try:
+            results = []
+            for doc in self.collection.find({"geo":{"$exists":True}}):
+                print doc
+                results.append(doc['geo'])
+            
+            return results
+        except:
+            return False
+
+
+if __name__ == '__main__':
+    total = len(sys.argv)
+
+    if total < 4:
+        print "Utilization: python traffic_aggregrate.py <mongo_host> <mongo_db> <mongo_collection> <query_field>"
+        exit(0)
+
+    mongo_db = {
+        'host' : str(sys.argv[1]),
+        'default_port' : 27017,
+        'db' : str(sys.argv[2]),
+        'collection' : str(sys.argv[3]),
+        'query_field' : str(sys.argv[4]),
+    }
+    
+    conn = mongo_host(mongo_db)
+
+    results = conn.get_traffic()
+    if results:
+        print results
